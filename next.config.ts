3c756b75fcc9b16336f4cb47c93a9d738147860c
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-11 02:23:23 +0000

    it dindt work

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'chapelhillmanagers.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
