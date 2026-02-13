46420eba8ed817671d6cf1ec88eaea9edb997549
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-13 22:57:42 +0000

    Demo Home Find a Job Browse Candidates Opportunities Companies Pricing B

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`}>
+      <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
