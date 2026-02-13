26ec6d78505f8638ef75e3b0b2765383198f93b6
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-13 12:13:58 +0000

    images are still broken

--- next.config.ts
+++ next.config.ts
@@ -29,11 +29,14 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
 
 export default nextConfig;
-
-    
-    
