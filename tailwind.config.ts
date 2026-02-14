7589e0f3c660d3599d51bfea3a4d9933462a4979
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-02-14 19:36:36 +0000

    make menu operating for menu and sub menu move smootly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -125,8 +125,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down 0.3s ease-out',
+        'accordion-up': 'accordion-up 0.3s ease-out',
         'marquee-rtl': 'marquee-rtl 40s linear infinite',
         'marquee-ltr': 'marquee-ltr 80s linear infinite',
         'gradient': 'gradient 3s linear infinite',
